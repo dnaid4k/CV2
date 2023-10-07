0019344e72e8aa622dd50181637d0ffba56b6432
Norbert.Duchnowski <dnaid4k@gmail.com>
2023-10-07 20:33:26 +0200

    progress plus installed advanced animations

--- portfolio/app/Components/Hero.jsx
+++ portfolio/app/Components/Hero.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -7,17 +10,58 @@ const Hero = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center">
         <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Portfolio Website</h1>
-        <p className='text-[#ADB7BE] text-lg lg:text-xl'>
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit.  
-        </p>
-        </div>
-        <div className="col-span-5">
-          <Image
-            src="/images/profilo.jpg"
-            alt='hero image'
-            width={200}
-            height={300}
+        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Norbert",
+                1000,
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "UI/UX Designer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
             />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatum.
+          </p>
+          <div>
+            <Link
+              href="/contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="rounded-lg bg-[#194e38] w-[300px] h-[400px] lg:w-[300px] lg:h-[400px] relative">
+            <Image
+              src="/images/profilo.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
